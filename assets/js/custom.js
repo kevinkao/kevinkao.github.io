8f20a42d8fbb5afedd2927bce17662db2b1d4227
kevinkao <kevin@ifalo.com.tw>
2017-07-12 00:06:52 +0800

    updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,12 @@
 
 $(function () {
 
-    console.log($('#backstretch'));
     $('#intro').backstretch(['assets/img/b1.jpg']);
+
+    $('.rotate').textrotator({
+        animation: "dissolve",
+        separator: "|",
+        speed: 3000
+    });
 
 });
