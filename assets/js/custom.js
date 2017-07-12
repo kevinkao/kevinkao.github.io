c78cb42c0cb77e045742f688bf293bc868edbc51
kevinkao <kevin@ifalo.com.tw>
2017-07-12 23:20:25 +0800

    updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,28 @@
 
 $(function () {
 
+    $('body').scrollspy({
+        target: '.navbar-custom',
+        offset: 50
+    });
+
     $('#intro').backstretch(['assets/img/b1.jpg']);
 
     $('.rotate').textrotator({
         animation: "dissolve",
         separator: "|",
         speed: 3000
+    });
+
+    var $navbar = $('.navbar');
+    var navHeight = $navbar.height();
+    console.log(navHeight);
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= navHeight) {
+            $navbar.addClass('navbar-color');
+        } else {
+            $navbar.removeClass('navbar-color');
+        }
     });
 
 });
